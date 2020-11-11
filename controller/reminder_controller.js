bb0388ae8cbec24f4d91534d30360eaec96b202d
dlatlrrb <dlatlrrb@gmail.com>
2020-11-10 21:06:25 -0800

    updatefeature

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -3,7 +3,9 @@ let database = require("../database");
 let remindersController = {
   // Show a list of reminders
   list: (req, res) => {
-    res.render('reminder/index', { reminders: database.cindy.reminders })
+    res.render('reminder/index', {
+      reminders: database.cindy.reminders
+    })
   },
 
   // Show a Create Reminder Page
@@ -18,9 +20,13 @@ let remindersController = {
       return reminder.id == reminderToFind;
     })
     if (searchResult != undefined) {
-      res.render('reminder/single-reminder', { reminderItem: searchResult })
+      res.render('reminder/single-reminder', {
+        reminderItem: searchResult
+      })
     } else {
-      res.render('reminder/index', { reminders: database.cindy.reminders })
+      res.render('reminder/index', {
+        reminders: database.cindy.reminders
+      })
     }
   },
 
@@ -40,17 +46,43 @@ let remindersController = {
   // Show the Edit Reminder Page
   edit: (req, res) => {
     // ⭐️ your implementation here ⭐️
-
+    let reminderToFind = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    })
+    res.render('reminder/edit', {
+      reminderItem: searchResult
+    })
   },
 
   // Edit the Reminder
   update: (req, res) => {
     // ⭐️ your implementation here ⭐️
+    let reminderToFind = req.params.id;
+    database.cindy.reminders.forEach(item => {
+      if (item.id == reminderToFind) {
+        item.title = req.body.title
+        item.description = req.body.description
+        item.completed = Boolean(req.body.completed)
+      }
+      res.redirect('/reminders')
+    })
   },
 
   // Delete the Reminder
   delete: (req, res) => {
     // ⭐️ your implementation here ⭐️
+    let reminderToFind = req.params.id; // returns the id of remindern ie http://localhost:3000/reminder/1 **id=1**
+    //remove reminder with id of reminderTOFind
+
+    database.cindy.reminders.forEach(element => { // loop through the list of reminders
+      const index = database.cindy.reminders.indexOf(element); // returns the index of the reminder from the list
+
+      if (element.id == Number(reminderToFind)) {
+        database.cindy.reminders.splice(index, 1)
+      }
+    })
+    res.redirect('/reminders');
   }
 }
 
